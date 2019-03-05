596b2f727cb2d6609063e54ab9f8926d1b00d979
Bun Mengnguon <mengnguon@icloud.com>
2019-03-05 15:57:43 +0700

    Create rails application

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root to: 'welcome#home'
+
+  get 'welcome/home', to: 'welcome#home'
+
+  get 'welcome/about', to: 'welcome#about'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
